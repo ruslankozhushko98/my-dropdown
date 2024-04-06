f0999684c989e7acb12132540c2eefd1d7f83847
ruslankozhushko98 <ruslankozhushko98@gmail.com>
2024-04-05 23:04:03 -0400

    feat: initial commit;

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'comma-spacing': ['error', { after: true }],
+    'object-curly-spacing': ['error', 'always'],
+    'eol-last': ['error', 'always'],
+    'no-multi-spaces': 'error',
+    'max-lines': ['error', 150],
+    'no-multiple-empty-lines': ['error', { max: 1 }],
+    eqeqeq: 'error',
+  },
+}
